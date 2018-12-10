bfda8eff7a1a872e255ba60917880a0ab41152df
DBankiewicz <31695722+DBankiewicz@users.noreply.github.com>
2018-12-10 16:04:09 +0100

    Add files via upload

---
+++ FindImage.java
@@ -0,0 +1,138 @@
+package imageProcessing;
+import javafx.scene.Group;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FindImage {
+
+    Color[][] target;
+    int maxX;
+    int maxY;
+    Random gen;
+
+
+    public FindImage(Color[][] img, int x, int y ){
+        target = img;
+        maxX = x;
+        maxY = y;
+        gen = new Random();
+    }
+
+    //Calculate polygon vector error
+    public double calculatePolyVectorError( List<ConvexPolygon> ls){
+        Group image = new Group();
+        for (ConvexPolygon p : ls){
+            image.getChildren().add(p);
+        }
+
+        WritableImage wimg = new WritableImage(maxX, maxY);
+        image.snapshot(null, wimg);
+        PixelReader pr = wimg.getPixelReader();
+
+        double res=0;
+        for (int i=0;i<maxX;i++){
+            for (int j=0;j<maxY;j++){
+                Color c = pr.getColor(i, j);
+                res += Math.pow(c.getBlue()-target[i][j].getBlue(),2)
+                        +Math.pow(c.getRed()-target[i][j].getRed(),2)
+                        +Math.pow(c.getGreen()-target[i][j].getGreen(),2);
+            }
+        }
+        return Math.sqrt(res);
+    }
+
+    //Mutate image
+    public List<ConvexPolygon> mutateImage(List<ConvexPolygon> p, int step){
+        List<ConvexPolygon> mutated = new ArrayList<>();
+        int k = gen.nextInt(50);
+        for(int i = 0; i < 50; i++){
+            if(i != k)
+                mutated.add(p.get(i));
+            else{
+                int g = gen.nextInt(5);
+
+                switch (g) {
+                    //change of opacity
+                    case 0:
+                        ConvexPolygon mutation =p.get(i).clone();
+                        mutation.setOpacity(gen.nextDouble());
+                        mutated.add(mutation);
+                        break;
+                    //change of color red
+                    case 1:
+                        mutation = p.get(i).clone();
+                        Color mutColor = (Color) mutation.getFill();
+                        double blue = mutColor.getBlue();
+                        double green = mutColor.getGreen();
+                        mutation.setFill(Color.color(gen.nextDouble(), green, blue ));
+                        mutated.add(mutation);
+                        break;
+                    //change of green color
+                    case 2:
+                        mutation = p.get(i).clone();
+                        mutColor = (Color) mutation.getFill();
+                        double red = mutColor.getRed();
+                        blue = mutColor.getBlue();
+                        mutation.setFill(Color.color(red, gen.nextDouble(), blue ));
+                        mutated.add(mutation);
+                        break;
+                    //change of blue color
+                    case 3:
+                        mutation = p.get(i).clone();
+                        mutColor = (Color) mutation.getFill();
+                        red = mutColor.getRed();
+                        green = mutColor.getGreen();
+                        mutation.setFill(Color.color(red, green, gen.nextDouble() ));
+                        mutated.add(mutation);
+                        break;
+                    case 4:
+                        //move around triangle a little
+                        if(p.get(i).getPoints().size() == 6 ){
+                            ConvexPolygon clo = p.get(i).clone();
+                            int point = gen.nextInt(6);
+                            double val = clo.getPoints().get(point);
+                            clo.getPoints().set(point, val + Math.pow(-1, gen.nextInt(2))*gen.nextDouble() * step);
+                            mutated.add(clo);
+                        }
+                        //new polygon with the same color
+                        else{
+                            mutation = new ConvexPolygon(7);
+                            mutation.setFill(p.get(i).getFill());
+                            mutated.add(mutation);
+                        }
+                        break;
+                    //no mutation
+                    default:
+                        System.out.println("DEFAULT");
+                        mutated.add(p.get(i).clone());
+                        break;
+                }
+            }
+        }
+        return mutated;
+    }
+
+    //Approximate
+    public List<ConvexPolygon> approximate(List<ConvexPolygon> p, double goal, int step){
+        double error = calculatePolyVectorError(p);
+        int generation = 0, round = 0, total = 0;
+        while(error > goal){
+            List<ConvexPolygon> mutated = mutateImage(p, step);
+            double e = calculatePolyVectorError(mutated);
+            if(e < error){
+                total += round;
+                generation += 1;
+                System.out.println(generation+"Error "+e+" "+round+" "+total);
+                error = e;
+                p = mutated;
+                round = 0;
+            }
+            round += 1;
+        }
+        return p;
+    }
+}
---
+++ Test.java
@@ -0,0 +1,136 @@
+package imageProcessing;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+
+public class Test extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage myStage) {
+        String targetImage = "monaLisa-100.jpg";
+        Color[][] target = null;
+        int maxX = 0;
+        int maxY = 0;
+        try {
+            BufferedImage bi = ImageIO.read(new File(targetImage));
+            maxX = bi.getWidth();
+            maxY = bi.getHeight();
+            ConvexPolygon.max_X = maxX;
+            ConvexPolygon.max_Y = maxY;
+            target = new Color[maxX][maxY];
+            for (int i = 0; i < maxX; i++) {
+                for (int j = 0; j < maxY; j++) {
+                    int argb = bi.getRGB(i, j);
+                    int b = (argb) & 0xFF;
+                    int g = (argb >> 8) & 0xFF;
+                    int r = (argb >> 16) & 0xFF;
+                    int a = (argb >> 24) & 0xFF;
+                    target[i][j] = Color.rgb(r, g, b);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+            System.exit(9);
+        }
+        System.out.println("Read target image " + targetImage + " " + maxX + "x" + maxY);
+
+
+        Random gen = new Random();
+
+        FindImage sol = new FindImage(target, maxX, maxY);
+        List<ConvexPolygon> p = new ArrayList<>();
+        for(int i =0; i < 50; i++){
+            p.add(new ConvexPolygon(3));
+        }
+        saveTest(p, maxX, maxY, myStage);
+        int step = 30;
+        for(int goal = 85; goal > 0; goal -= 5){
+            System.out.println("GOAL "+goal);
+            List<ConvexPolygon> best = sol.approximate(p, goal, step);
+            saveResult(best, maxX, maxY, myStage);
+            p = best;
+            if(goal < 25)
+                goal += 3;
+        }
+
+
+    }
+
+    //Save and show an image as "test"
+    private void saveTest(List<ConvexPolygon> ls, int maxX, int maxY, Stage myStage){
+        Group image = new Group();
+        for (ConvexPolygon p : ls)
+            image.getChildren().add(p);
+
+        // Calcul de la couleur de chaque pixel.Pour cela, on passe par une instance de
+        // WritableImage, qui possède une méthode pour obtenir un PixelReader.
+        WritableImage wimg = new WritableImage(maxX,maxY);
+        image.snapshot(null,wimg);
+        PixelReader pr = wimg.getPixelReader();
+
+        // Stockage de l'image dans un fichier .png
+        RenderedImage renderedImage = SwingFXUtils.fromFXImage(wimg, null);
+        try {
+            ImageIO.write(renderedImage, "png", new File("test.png"));
+            System.out.println("wrote image in " + "test.png");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        // affichage de l'image dans l'interface graphique
+        Scene scene = new Scene(image, maxX, maxY);
+        myStage.setScene(scene);
+        myStage.show();
+
+    }
+
+    //Save and show an image as "result"
+    private void saveResult(List<ConvexPolygon> ls, int maxX, int maxY, Stage myStage){
+        Group image = new Group();
+        for (ConvexPolygon p : ls)
+            image.getChildren().add(p);
+
+        // Calcul de la couleur de chaque pixel.Pour cela, on passe par une instance de
+        // WritableImage, qui possède une méthode pour obtenir un PixelReader.
+        WritableImage wimg = new WritableImage(maxX,maxY);
+        image.snapshot(null,wimg);
+        PixelReader pr = wimg.getPixelReader();
+
+        // Stockage de l'image dans un fichier .png
+        RenderedImage renderedImage = SwingFXUtils.fromFXImage(wimg, null);
+        try {
+            ImageIO.write(renderedImage, "png", new File("result_.png"));
+            System.out.println("wrote image in " + "result_.png");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        // affichage de l'image dans l'interface graphique
+        Scene scene2 = new Scene(image, maxX, maxY);
+        myStage.setScene(scene2);
+        myStage.show();
+
+    }
+
+}
